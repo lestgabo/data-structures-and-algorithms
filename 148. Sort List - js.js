75ae858bf2f56e301e099f8bbab7dd97b5f9cbf0
lestgabo <lestgabo@gmail.com>
2020-12-14 20:55:46 -0800

    Add 148. Sort List

--- 148. Sort List - js.js
+++ 148. Sort List - js.js
@@ -38,10 +38,65 @@ Constraints:
  * @return {ListNode}
  */
 
-const solution = (head) => {
-    console.log(head);
-    if (head === null) return head;
-    console.log();
+const sortList = (head) => {
+    // console.log(head);
+    if (head === null || head.next === null) return head;
+
+    // use merge sort on the linked list
+    // split the list
+    const [left, right] = split(head);
+    // console.log('left: ', left);
+    // console.log('right: ', right);
+
+    // temp node to link all the sorted nodes
+    const root = new ListNode(null);
+    return merge(root, sortList(left), sortList(right));
+};
+
+const split = (node) => {
+    // use slow & fast pointer to find middle node
+    // splits the list into list[0, slow] and list[slow+1, list.length]
+    let slow = node;
+    let fast = node;
+    // when fast finds null, slow will be at mid
+    while (fast.next && fast.next.next) {
+        slow = slow.next;
+        fast = fast.next.next;
+    }
+    const left = node;
+    const right = slow.next;
+    // cut off slow so that left is not connected to right
+    slow.next = null;
+
+    return [left, right];
+};
+
+const merge = (root, left, right) => {
+    // merge the smaller node in the left and right list first
+    let pointer = root;
+    while (left !== null || right !== null) {
+        // either left or right is exhausted
+        if (left === null) {
+            pointer.next = right;
+            right = right.next;
+        } else if (right === null) {
+            pointer.next = left;
+            left = left.next;
+        }
+        // left and right still have nodes
+        else {
+            if (left.val < right.val) {
+                pointer.next = left;
+                left = left.next;
+            } else {
+                pointer.next = right;
+                right = right.next;
+            }
+        }
+        pointer = pointer.next;
+    }
+    // return the second node in the list because the first is a temporary node
+    return root.next;
 };
 
 function ListNode(val, next) {
@@ -55,4 +110,4 @@ head.next.next = new ListNode(1);
 head.next.next.next = new ListNode(3);
 // head.next.next.next.next = new ListNode(5);
 
-console.log(solution(head));
+console.log(sortList(head));
