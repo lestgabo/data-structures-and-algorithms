da6963f5293bc90a1bda4fbd4c6dc70e4fe717ac
lestgabo <lestgabo@gmail.com>
2020-12-16 03:34:03 -0800

    Update 252. Meeting Rooms - easier to udnerstand

--- 252. Meeting Rooms - js.js
+++ 252. Meeting Rooms - js.js
@@ -1,8 +1,10 @@
 /**
- * 252. Meeting Rooms
-Easy
+ ** 252. Meeting Rooms
+ ** Easy
+ ** https://leetcode.com/problems/meeting-rooms/
 
-Given an array of meeting time intervals consisting of start and end times [[s1,e1],[s2,e2],...] (si < ei), determine if a person could attend all meetings.
+Given an array of meeting time intervals consisting of start and 
+end times [[s1,e1],[s2,e2],...] (si < ei), determine if a person could attend all meetings.
 
 Example 1:
 
@@ -20,38 +22,25 @@ https://leetcode.com/problems/meeting-rooms/
  */
 
 const solution = (intervals) => {
-    let minimum = Number.MAX_VALUE;
-    let maximum = -Number.MAX_VALUE;
-
-    intervals.sort((a, b) => {
-        return a[0] - b[0];
-    });
-    console.log('intervals ->', intervals);
-    for (let i = 0; i < intervals.length; i++) {
-        let [start, end] = intervals[i];
-        // console.log('start ->', start);
-        // console.log('end ->', end);
-        if (start < maximum) return false;
-        minimum = Math.min(minimum, start);
-        maximum = Math.max(maximum, end);
+    intervals.sort((a, b) => a[0] - b[0]);
+    // console.log(intervals)
+    for (let i = 0; i < intervals.length - 1; i++) {
+        if (intervals[i][1] > intervals[i + 1][0]) return false;
     }
-    // console.log('minimum ->', minimum);
-    // console.log('maximum ->', maximum);
-
     return true;
 };
 
-let intervals = [
-    [22, 27],
-    [2, 33],
-];
+// let intervals = [
+//     [22, 27],
+//     [2, 33],
+// ];
 // let intervals = [
 //     [7, 10],
 //     [2, 4],
 // ];
-// let intervals = [
-//     [0, 30],
-//     [5, 10],
-//     [15, 20],
-// ];
+let intervals = [
+    [0, 3],
+    [5, 10],
+    [15, 20],
+];
 console.log(solution(intervals));
