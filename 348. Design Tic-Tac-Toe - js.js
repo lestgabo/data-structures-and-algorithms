5cf1dad86ae0eb0737b97ddc2aa5a193d6624b93
lestgabo <lestgabo@gmail.com>
2020-12-15 19:55:09 -0800

    Add 348. Design Tic-Tac-Toe

---
+++ 348. Design Tic-Tac-Toe - js.js
@@ -0,0 +1,184 @@
+/**
+ ** 348. Design Tic-Tac-Toe
+ ** Medium
+ ** https://leetcode.com/problems/design-tic-tac-toe/
+
+Assume the following rules are for the tic-tac-toe game on an n x n board between two players:
+
+    A move is guaranteed to be valid and is placed on an empty block.
+    Once a winning condition is reached, no more moves are allowed.
+    A player who succeeds in placing n of their marks in a horizontal, vertical, or diagonal row wins the game.
+
+Implement the TicTacToe class:
+
+    TicTacToe(int n) Initializes the object the size of the board n.
+    int move(int row, int col, int player) Indicates that player with id player plays at the cell (row, col) of the board. The move is guaranteed to be a valid move.
+
+Follow up:
+Could you do better than O(n2) per move() operation?
+
+ 
+
+Example 1:
+
+Input
+["TicTacToe", "move", "move", "move", "move", "move", "move", "move"]
+[[3], [0, 0, 1], [0, 2, 2], [2, 2, 1], [1, 1, 2], [2, 0, 1], [1, 0, 2], [2, 1, 1]]
+Output
+[null, 0, 0, 0, 0, 0, 0, 1]
+
+Explanation
+TicTacToe ticTacToe = new TicTacToe(3);
+Assume that player 1 is "X" and player 2 is "O" in the board.
+ticTacToe.move(0, 0, 1); // return 0 (no one wins)
+|X| | |
+| | | |    // Player 1 makes a move at (0, 0).
+| | | |
+
+ticTacToe.move(0, 2, 2); // return 0 (no one wins)
+|X| |O|
+| | | |    // Player 2 makes a move at (0, 2).
+| | | |
+
+ticTacToe.move(2, 2, 1); // return 0 (no one wins)
+|X| |O|
+| | | |    // Player 1 makes a move at (2, 2).
+| | |X|
+
+ticTacToe.move(1, 1, 2); // return 0 (no one wins)
+|X| |O|
+| |O| |    // Player 2 makes a move at (1, 1).
+| | |X|
+
+ticTacToe.move(2, 0, 1); // return 0 (no one wins)
+|X| |O|
+| |O| |    // Player 1 makes a move at (2, 0).
+|X| |X|
+
+ticTacToe.move(1, 0, 2); // return 0 (no one wins)
+|X| |O|
+|O|O| |    // Player 2 makes a move at (1, 0).
+|X| |X|
+
+ticTacToe.move(2, 1, 1); // return 1 (player 1 wins)
+|X| |O|
+|O|O| |    // Player 1 makes a move at (2, 1).
+|X|X|X|
+
+ 
+
+Constraints:
+
+    2 <= n <= 100
+    player is 1 or 2.
+    1 <= row, col <= n
+    (row, col) are unique for each different call to move.
+    At most n2 calls will be made to move.
+
+
+ */
+
+/**
+ * Initialize your data structure here.
+ * @param {number} n
+ */
+var TicTacToe = function (n) {
+    this.len = n;
+    this.row = {};
+    this.col = {};
+    this.diag = 0;
+    this.revDiag = 0;
+};
+
+/**
+ * Player {player} makes a move at ({row}, {col}).
+        @param row The row of the board.
+        @param col The column of the board.
+        @param player The player, can be either 1 or 2.
+        @return The current winning condition, can be either:
+                0: No one wins.
+                1: Player 1 wins.
+                2: Player 2 wins. 
+ * @param {number} row 
+ * @param {number} col 
+ * @param {number} player
+ * @return {number}
+ */
+TicTacToe.prototype.move = function (row, col, player) {
+    /**
+     * 
+     * We keep sum for every row, column, diagonal, reverse diagonal. No need to have the actual board in memory.
+    For player one move, we can add 1 to the sum and for player 2 we can deduct 1;
+    When any sum reaches the size of the board we return the winning player.
+     */
+    // console.log('row: ', row);
+    // console.log('col: ', col);
+    // console.log('player: ', player);
+
+    let playerValue;
+    if (player === 1) {
+        playerValue = 1;
+    } else {
+        playerValue = -1;
+    }
+
+    if (this.row[row]) {
+        this.row[row] += playerValue;
+    } else {
+        this.row[row] = playerValue;
+    }
+    if (this.col[col]) {
+        this.col[col] += playerValue;
+    } else {
+        this.col[col] = playerValue;
+    }
+    if (row === col) this.diag += playerValue;
+    if (row + col === this.len - 1) this.revDiag += playerValue;
+
+    // found winner because either player 1 or 2 reached either: 3 row, 3 col, 3 diag, 3 revDiag
+    if (
+        Math.abs(this.row[row]) === this.len ||
+        Math.abs(this.col[col]) === this.len ||
+        Math.abs(this.diag) === this.len ||
+        Math.abs(this.revDiag) === this.len
+    ) {
+        return player;
+    }
+    // no player won yet
+    return 0;
+
+    // const i = player === 1 ? 1 : -1;
+
+    // this.row[row] += i;
+    // this.col[col] += i;
+    // if (row === col) this.diag += i;
+    // if (col === this.len - row - 1) this.revDiag += i;
+
+    // if (
+    //     Math.abs(this.row[row]) === this.len ||
+    //     Math.abs(this.col[col]) === this.len ||
+    //     Math.abs(this.diag) === this.len ||
+    //     Math.abs(this.revDiag) === this.len
+    // )
+    //     return player;
+
+    // return 0;
+};
+
+/**
+ * Your TicTacToe object will be instantiated and called as such:
+ * var obj = new TicTacToe(n)
+ * var param_1 = obj.move(row,col,player)
+ */
+
+['TicTacToe', 'move', 'move', 'move', 'move', 'move', 'move', 'move'];
+let input = [[3], [0, 0, 1], [0, 2, 2], [2, 2, 1], [1, 1, 2], [2, 0, 1], [1, 0, 2], [2, 1, 1]];
+const start = new TicTacToe(input[0][0]);
+console.log('input[0][0]: ', input[0][0]);
+for (let i = 1; i < input.length; i++) {
+    console.log('input[i]: ', input[i]);
+
+    const [row, col, player] = input[i];
+
+    console.log(start.move(row, col, player));
+}
--- 417. Pacific Atlantic Water Flow 2 - js.js
+++ 417. Pacific Atlantic Water Flow 2 - js.js
@@ -1,10 +1,14 @@
 /**
- * 417. Pacific Atlantic Water Flow
-Medium
+    **  417. Pacific Atlantic Water Flow
+    ** Medium
+    ** https://leetcode.com/problems/pacific-atlantic-water-flow/
 
-Given an m x n matrix of non-negative integers representing the height of each unit cell in a continent, the "Pacific ocean" touches the left and top edges of the matrix and the "Atlantic ocean" touches the right and bottom edges.
+Given an m x n matrix of non-negative integers representing the height of each unit cell in a continent, 
+the "Pacific ocean" touches the left and top edges of the matrix and the "Atlantic ocean" 
+touches the right and bottom edges.
 
-Water can only flow in four directions (up, down, left, or right) from a cell to another one with height equal or lower.
+Water can only flow in four directions (up, down, left, or right) 
+from a cell to another one with height equal or lower.
 
 Find the list of grid coordinates where water can flow to both the Pacific and Atlantic ocean.
 
@@ -31,7 +35,7 @@ Return:
 
 [[0, 4], [1, 3], [1, 4], [2, 2], [3, 0], [3, 1], [4, 0]] (positions with parentheses in above matrix).
 
-https://leetcode.com/problems/pacific-atlantic-water-flow/
+
  */
 
 /**
@@ -58,7 +62,11 @@ const pacificAtlantic = (matrix) => {
         // bounds and water constraints
         if (i < 0 || i > matrix.length - 1 || j < 0 || j > matrix[0].length - 1) {
             return;
-        } else if (matrix[i][j] < pastValue) {
+        } else if (pastValue >= matrix[i][j]) {
+            // means: the water only goes to the next grid if
+            // the current Value (matrix[i][j]) is greater or equal to pastValue
+            // because we imagine from that spot, water trickles DOWN to lower or equal values
+            // but we are doing the opposite, going through water's path
             return;
         } else if (ocean[i][j] === 1) {
             return;
@@ -88,8 +96,8 @@ const pacificAtlantic = (matrix) => {
         dfs(matrix, 0, rowLength - 1, j, atlantic);
     }
 
-    // console.log(pacific);
-    // console.log(atlantic);
+    console.log('pacific: ', pacific);
+    console.log('atlantic: ', atlantic);
     let answer = [];
     for (let i = 0; i < rowLength; i++) {
         for (let j = 0; j < colLength; j++) {
@@ -98,21 +106,22 @@ const pacificAtlantic = (matrix) => {
             }
         }
     }
+
     return answer;
 };
 
-let matrix = [
-    [3, 3, 3],
-    [3, 1, 3],
-    [0, 2, 4],
-]; //[[0,0],[0,1],[0,2],[1,0],[1,2],[2,0],[2,1],[2,2]]
-// let matrix = [];
 // let matrix = [
-//     [1, 2, 2, 3, 5],
-//     [3, 2, 3, 4, 4],
-//     [2, 4, 5, 3, 1],
-//     [6, 7, 1, 4, 5],
-//     [5, 1, 1, 2, 4],
-// ];
+//     [3, 3, 3],
+//     [3, 1, 3],
+//     [0, 2, 4],
+// ]; //[[0,0],[0,1],[0,2],[1,0],[1,2],[2,0],[2,1],[2,2]]
+// let matrix = [];
+let matrix = [
+    [1, 2, 2, 3, 5],
+    [3, 2, 3, 4, 4],
+    [2, 4, 5, 3, 1],
+    [6, 7, 1, 4, 5],
+    [5, 1, 1, 2, 4],
+];
 // let matrix = [[1,2,3,4,5,6,7,8,9,10,11,12],[44,45,46,47,48,49,50,51,52,53,54,13],[43,80,81,82,83,84,85,86,87,88,55,14],[42,79,108,109,110,111,112,113,114,89,56,15],[41,78,107,128,129,130,131,132,115,90,57,16],[40,77,106,127,140,141,142,133,116,91,58,17],[39,76,105,126,139,144,143,134,117,92,59,18],[38,75,104,125,138,137,136,135,118,93,60,19],[37,74,103,124,123,122,121,120,119,94,61,20],[36,73,102,101,100,99,98,97,96,95,62,21],[35,72,71,70,69,68,67,66,65,64,63,22],[34,33,32,31,30,29,28,27,26,25,24,23]]
-console.log(waterFlow(matrix));
+console.log(pacificAtlantic(matrix));
