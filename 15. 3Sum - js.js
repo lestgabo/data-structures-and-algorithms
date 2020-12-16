9dcef43158e8acece56bf8a0f69a4aca063afa95
lestgabo <lestgabo@gmail.com>
2020-12-16 01:54:43 -0800

    Add 15. 3Sum - cool way (3 pointers)

--- 15. 3Sum - js.js
+++ 15. 3Sum - js.js
@@ -38,7 +38,7 @@ Constraints:
  */
 
 //  TLE
-var threeSum = function (nums) {
+var threeSum2 = function (nums) {
     // brute force, 3 loops
     let resultCheck = new Set();
     let result = [];
@@ -69,5 +69,55 @@ var threeSum = function (nums) {
     return result;
 };
 
+const threeSum = (nums) => {
+    // do 3 pointers, i j k -> i as the anchor and j and k moving fast
+    // j to the very left, k to the very right
+    // sort nums: O(NlogN) ascending helps to remove duplicates
+    // while loop inside i for loop, it will loop while j < k
+    // inside the while, check for the sum === target
+    // if yes: push nums[i] [j] [k] into result
+    //         check if next j is a duplicate if yes: increment
+    //         check if next k is a duplicate if yes: decrement
+    //         then increment j, decrement k (to feed the while loop)
+    // else if (target < sum): increment j (remember we are in a sorted array)
+    // else (target > sum): decrement k
+
+    let target = 0;
+    let result = [];
+    nums.sort((a, b) => a - b);
+    for (let i = 0; i < nums.length - 2; i++) {
+        let j = i + 1;
+        let k = nums.length - 1;
+        let partialTarget = target - nums[i];
+
+        // break because positive numbers cant add to negative number
+        if (nums[i] > 0) break;
+        // removes duplicates of i
+        if (i > 0 && nums[i] === nums[i - 1]) continue;
+
+        while (j < k) {
+            let partialSum = nums[j] + nums[k];
+            if (partialSum === partialTarget) {
+                result.push([nums[i], nums[j], nums[k]]);
+
+                // removes duplicates of j and k
+                while (nums[j] === nums[j + 1]) j++;
+                while (nums[k] === nums[k - 1]) k--;
+
+                // feed the while loop
+                j++;
+                k--;
+            } else if (partialSum < partialTarget) {
+                // e.g. 5 < 10, num[j]+num[k]=5 < 10 = partial target
+                j++;
+            } else {
+                k--;
+            }
+        }
+    }
+    return result;
+};
+
 const nums = [-1, 0, 1, 2, -1, -4]; //Output: [[-1,-1,2],[-1,0,1]]
+console.log(nums.sort((a, b) => a - b));
 console.log(threeSum(nums));
