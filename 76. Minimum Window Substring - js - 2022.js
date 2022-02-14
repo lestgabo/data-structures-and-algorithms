50479c11d886f4db0a6f1ebb514f37173d2dd164
lestgabo <lestgabo@gmail.com>
2022-02-14 00:11:03 -0800

    Try 76. Minimum Window Substring
    129 / 266 test cases passed only

--- 76. Minimum Window Substring - js - 2022.js
+++ 76. Minimum Window Substring - js - 2022.js
@@ -50,8 +50,62 @@ Follow up: Could you find an algorithm that runs in O(m + n) time?
  */
  
 const minWindow = (s, t) => {
-    
+    let tSet = new Set(t);
+
+    if (s.length === 0 || s === "") return "";
+    if (t.length > s.length) return "";
+  
+    let left = 0;
+    let right = s.length - 1;
+    let minimumWindow = "";
+
+    for (let i = 0; i < s.length; i++) {
+        let char = s[i];
+        let window = s.slice(left, right+1);
+        let tempT = t.split('');
+
+        // console.log('char: ', char)
+        // console.log('left: ', left)
+        // console.log('window: ', window)
+        // console.log('minimumWindow: ', minimumWindow)
+        // console.log('tempT: ', tempT)
+
+        // check for t inside window substring
+        for (let j = 0; j < window.length; j++) {
+            let windowChar = window[j];
+
+            console.log('windowChar: ', windowChar)
+            console.log('tempT.includes(windowChar): ', tempT.includes(windowChar))
+
+            if (tempT.includes(windowChar)) { 
+                tempT.splice(tempT.indexOf(windowChar), 1);
+            }
+
+            console.log('tempT during:' ,tempT)
+        }
+
+        console.log('tempT after: ', tempT)
+
+        if (Array.isArray(tempT) && !tempT.length) {
+            // t is found inside window substring so it counts
+           
+            // console.log('window.length <= minimumWindow:' , window.length <= minimumWindow.length)
+            // there was a t inside a sunbstring and we are setting minWindow as size s
+            minimumWindow = s;
+            if (window.length <= minimumWindow.length) minimumWindow = window;
+        }
+
+        left++;
+
+        // console.log('minimumWindow: ', minimumWindow)
+        // console.log('*****************')
+    }
+    return minimumWindow;
 };
 
-Input: s = "ADOBECODEBANC", t = "ABC"
+Input: s = "ab", t = "b"
+// Input: s = "a", t = "b"
+// Input: s = "a", t = "aa"
+// Input: s = "a", t = "a"
+// Input: s = "ADOBECODEBANC", t = "ABC"
 console.log(minWindow(s, t));
